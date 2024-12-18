b88051661cb63257c0ede9c0be7783d471e55949
randomstring0 <sandshowcase@gmail.com>
2024-12-18 20:30:53 +0700

    Update check.lua

--- src/check.lua
+++ src/check.lua
@@ -1,6 +1,6 @@
 local v='1.4.0'
-if G.vers then
-  if vers~=v then
+if _G.vers then
+  if _G.vers~=v then
     game:GetService("StarterGui"):SetCore("SendNotification",{
 	    Title = "pshade out of date", 
     	Text = "use the newest version of pshade on yt @Im_Patrick", 
